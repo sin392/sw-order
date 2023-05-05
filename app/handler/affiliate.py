1b1ce380e726ef8d75aa7fd53dcacb1723e78450
sin392 <sin392@gmail.com>
2023-05-05 14:36:38 +0000

    add db to handler

--- app/handler/affiliate.py
+++ app/handler/affiliate.py
@@ -2,9 +2,9 @@ from app.models import *
 from fastapi.responses import Response
 
 
-def do_get_affiliates():
+def do_get_affiliates(db):
     return Response()
 
 
-def do_post_affiliates(body: AffiliatesPostRequest):
+def do_post_affiliates(body: AffiliatesPostRequest, db):
     return Response()
--- app/handler/user.py
+++ app/handler/user.py
@@ -2,9 +2,9 @@ from app.models import *
 from fastapi.responses import Response
 
 
-def do_get_users():
+def do_get_users(db):
     return Response()
 
 
-def do_post_users(body: UsersPostRequest):
+def do_post_users(body: UsersPostRequest, db):
     return Response()
